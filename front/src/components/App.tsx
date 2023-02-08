bcaaab85e0d3d42c10c68629ac3034f165f8bda4
tokun4869 <ba.tokun4869@gmail.com>
2023-02-08 13:55:17 +0900

    ヘッダーの作成

--- front/src/components/App.tsx
+++ front/src/components/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
+import Header from "./Header";
 import "../styles/App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
